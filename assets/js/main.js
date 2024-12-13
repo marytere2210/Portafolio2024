6f26a998abd33b7836c8895ca2a4ffa878e51292
marytere2210 <maritere.18.mp@gmail.com>
2024-12-13 14:43:31 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 import parallax from './components/parallax.js';
 import activeMenu from './components/selected_menu.js';
-import languageCheck from './components/change_to_english.js';
+import languageCheck from './helpers/change_to_english.js';
 import updateCopyRightYear from './helpers/date_updater.js';
 import loader from './helpers/loader.js';
 import resetToHome from './helpers/reload_page.js';
